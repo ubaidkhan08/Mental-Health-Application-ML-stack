7900232eb560e2225a42c2b8a45076a6724c2c64
Ubaid Khan <88443025+ubaidkhan08@users.noreply.github.com>
2023-04-16 07:23:41 +0530

    Update Streamlit.py

--- App/Streamlit.py
+++ App/Streamlit.py
@@ -5,12 +5,12 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 # Load model
-model = tf.keras.models.load_model('App/model.h5')
+model = tf.keras.models.load_model('model.h5')
 
 # Load data
 notes_df = pd.DataFrame({'note': ['']*30, 'sentiment': [0.6]*30})
 
-with open('App/tokenizer.json', 'r', encoding='utf-8') as f:
+with open('tokenizer.json', 'r', encoding='utf-8') as f:
     tokenizer_json = f.read()
 tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(tokenizer_json)
 
@@ -24,12 +24,15 @@ def predict_sentiment(text):
     text_padded = pad_sequences(text_sequence, padding='post', maxlen=100)
     return model.predict(text_padded)[0]
 
+
+
 def add_and_view_notes():
     global notes_df
     with st.container():
         st.write("## Notes:")
         consecutive_days = 0
         total_days = 0
+        alert_displayed = False
         for i in range(0, 30):
             with st.expander(f"April {i+1}"):
                 note = st.text_area("Add a note:", key=f"note_{i}")
@@ -38,24 +41,21 @@ def add_and_view_notes():
                     notes_df.loc[i, 'sentiment'] = predict_sentiment(note)
                     sentiment = notes_df.loc[i, 'sentiment']
                     color = 'red' if sentiment >= 0.6 else 'green'
-                    if sentiment >= 0.6:
-                        st.write("Self-harming (this won't be visible to the user)") # Displayed only if the alert is red
-                    elif sentiment < 0.6:
-                        st.write("Normal (this won't be visible to the user)") # Displayed only if the alert is green
-                sentiment = notes_df.loc[i, 'sentiment']
-                color = 'red' if sentiment >= 0.6 else 'green'
-                st.markdown(f"<p style='background-color: {color}'>{note}</p>", unsafe_allow_html=True)
-                if sentiment >= 0.6:
-                    consecutive_days += 1
-                    total_days += 1
-                    if consecutive_days >= 1 or total_days >= 1:
+                    st.markdown(f"<p style='background-color: {color}'>{note}</p>", unsafe_allow_html=True)
+                    if color == 'red' and not alert_displayed:
                         if st.button("Would you like to connect with our therapist?", key=f"alert_{i}"):
                             # Add code to connect with therapist here
                             st.write("Connecting with therapist...")
-                            consecutive_days = 0
+                            alert_displayed = True
+                    else:
+                        alert_displayed = False
                 else:
-                    consecutive_days = 0
-                    total_days += 1
+                    sentiment = predict_sentiment(note)
+                    notes_df.loc[i, 'sentiment'] = sentiment
+                    color = 'red' if sentiment >= 0.6 else 'green'
+                    st.markdown(f"<p style='background-color: {color}'>{note}</p>", unsafe_allow_html=True)
+
+
 
 
 # Set page title
@@ -63,7 +63,7 @@ st.set_page_config(page_title="Mental Health Platform")
 
 # Define app layout
 def app():
-    st.title("Cerina - Mental Health Caree")
+    st.title("Cerina - Mental Health Care")
     add_and_view_notes()
 
 if __name__ == "__main__":
