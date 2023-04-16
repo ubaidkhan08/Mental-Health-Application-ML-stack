cf973ad00ff9f55a60e9eba8c9dce8d263052784
Ubaid Khan <88443025+ubaidkhan08@users.noreply.github.com>
2023-04-16 19:13:36 +0530

    Update Streamlit.py

--- App/Streamlit.py
+++ App/Streamlit.py
@@ -49,8 +49,8 @@ def add_and_view_notes():
                         button_label = 'Would you like to connect with our therapist?'
                         
                         if st.button(button_label, key=f"alert_{i}"):
-                            link = 'https://cerina.co/#footer'
-                            st.markdown(f'<a href="{link}">{button_label}</a>', unsafe_allow_html=True)
+                            st.markdown(link, unsafe_allow_html=True)
+                            #st.markdown(f'<a href="{link}">{button_label}</a>', unsafe_allow_html=True)
                             st.write("Connecting with therapist...")
                 
                             alert_displayed = True
